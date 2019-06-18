c3bd232b057f50f7ba157b6143a549b23ad546ca
alex sagel <alex.sagel7587@gmail.com>
2019-06-17 23:12:37 -0400

    creating avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("images/profile.jpeg"),
+              ),
+              Text(
+                'Alex Sagel',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Software Developer',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.teal.shade200,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          )
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
